84bd6eba36ef0979a8ff01f99b7f0149b6a2fa9c
Marven Thieme <thieme@webhub.de>
2024-11-08 15:52:07 +0100

    Init

---
+++ bin/goodsoup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Generating the SBOM for composer packages
+echo "Generating the SBOM for composer packages..."
+composer CycloneDX:make-sbom --omit=dev --spec-version=1.3 --output-format=JSON --output-file="./sboms/sbom-composer.json"
+echo "Done."
+
+# Generating the SBOM for node packages
+echo "Generating the SBOM for node packages..."
+vendor/webhubworks/goodsoup/cyclonedx-npm  --spec-version=1.3 --output-format=json --output-file="./sboms/sbom-node.json"
+echo "Done."
+
+# Gathering the data and fill them into a sqlite database
+echo "TODO: Gathering the data and fill them into a sqlite database..."
